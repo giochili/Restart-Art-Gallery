b95041fdc4aa53059db79397f9e479d49346506a
gio gio <giochili2346@gmail.com>
2023-02-13 21:08:07 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+let btn = document.querySelector(".home-btn");
+
+btn.addEventListener("click", function () {
+  document.location.href = "index.html";
+});
---
+++ location.js
@@ -0,0 +1,6 @@
+"use strict";
+let btn = document.querySelector(".btn-container");
+
+btn.addEventListener("click", function () {
+  document.location.href = "location.html";
+});
